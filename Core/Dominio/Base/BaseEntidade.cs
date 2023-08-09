b2da1744fa750f2370d087478f27b06db37f290d
Jorge Edson <jorge.edson.bsi@gmail.com>
2023-08-09 04:00:35 -0300

    Criação da Biblioteca de classe que representa o nucleo da aplicação com o dominio

---
+++ Core/Dominio/Base/BaseEntidade.cs
@@ -0,0 +1,7 @@
+﻿namespace Desafio.Core.Dominio.Base
+{
+    public abstract class BaseEntidade
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Core/Dominio/Dispositivo.cs
@@ -0,0 +1,17 @@
+﻿using Desafio.Core.Dominio.Base;
+
+namespace Desafio.Core.Dominio
+{
+    public class Dispositivo : BaseEntidade
+    {
+        public string Nome { get; private set; }
+        public int Endereco { get; private set; }
+
+        public Dispositivo(string nome, int endereco)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Endereco = endereco;
+        }
+    }
+}
